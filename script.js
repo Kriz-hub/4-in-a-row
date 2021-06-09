3ee816456f3fe397e0e9f67740fef7079b8c0864
Kriz-hub <k.m.dikkeschei@casema.nl>
2021-06-09 19:12:46 +0000

    trying to get hovered element's ID, doesn't work now

--- script.js
+++ script.js
@@ -10,15 +10,31 @@ let col;
 var sceneDiv = document.getElementById("scene");
 var cubeDiv = document.getElementById("cube");
 var cubeCloneDiv;
+var count=0;
+var ident;
 for (col = 1; col<=totalCols; col++) {
     cubeCol += step;
     cubeRow = startCubeRow+step;
     for (row = 1; row<=totalRows; row++) {
+        count+=1;
         cubeRow -= step;
         cubeCloneDiv = cubeDiv.cloneNode(true);
         cubeCloneDiv.style.top = cubeRow + "em";
         cubeCloneDiv.style.left = cubeCol + "em";
+        cubeCloneDiv.id = "ident"+count;
         sceneDiv.appendChild(cubeCloneDiv);
     }
 }
+
+function dropBall(){
+let ballDiv = document.getElementById("redBall");
+let ballCloneDiv = ballDiv.cloneNode(true);
+}
+//https://stackoverflow.com/questions/21755937/accessing-an-element-that-is-currently-being-hovered-by-mouse-from-any-where-in
+document.querySelectorAll('div')[0].onmouseover = function(e){
+
+   var target = (e.target) ? e.target: e.srcElement;
+   alert(target.style.left);// get element that is being hovered over right now
+
+};
 
